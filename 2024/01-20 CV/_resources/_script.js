3f432eb51233d312ff7de22f941035ff2466f7bd
Frank Liang <frankliang@Franks-MacBook-Pro.local>
2025-05-13 01:17:52 +0100

    Built site for gh-pages

--- 2024/01-20 CV/_resources/_script.js
+++ 2024/01-20 CV/_resources/_script.js
@@ -1,8 +1,10 @@
 
 const role_mapping = {
     "show-all": "all",
-    "show-data-analyst": "data-analyst",
-    "show-data-engineer": "data-engineer"
+    "show-data-analyst": {
+        'any':["data-analyst"] },
+    "show-data-engineer": {
+        'any': ["data-engineer"] }
 }
 // Main event handler
 const handleRadioChange = async (event) => {
@@ -13,13 +15,28 @@ const handleRadioChange = async (event) => {
         document.querySelectorAll('div.taged-item').forEach(element => {
             const ele_tag_str = element.dataset.tags;
             const ele_tag = JSON.parse(ele_tag_str.replace(/'/g, '"'));
+            const ele_tooltip = ele_tag.join(", ");
+            
+            element.setAttribute('title', ele_tooltip);
+            element.setAttribute('data-toggle', "tooltip");
+            element.setAttribute('data-placement', "left");
+            element.setAttribute('tabindex', 0);
+
 
             if (tag == 'all') {
                 element.style.display = "block";
                 return;
             }
-            if (ele_tag.includes(tag) || ele_tag.includes('all') ) {
-                console.log(ele_tag);
+            let cond = true;
+            if (tag.any) {
+                const cond_any = tag.any.some(t => ele_tag.includes(t)) || ele_tag.includes('all')
+                cond &&= cond_any;
+            }
+            if (tag.all) {
+                const cond_all = tag.all.every(t => ele_tag.includes(t))
+                cond &&= cond_all;
+            }
+            if (cond) {
                 element.style.display = "block";
             } else {
                 element.style.display = "none";
